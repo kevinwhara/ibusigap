5c907d57f88b97e61c21f5616b12f7c458487541
Faznug_Space <153061199+FaishalFaiz@users.noreply.github.com>
2026-01-14 21:23:58 +0700

    setup color & alias

--- app/cek-masalah/page.jsx
+++ app/cek-masalah/page.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function CekMasalah() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex min-h-screen items-center justify-center font-sans">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
                 <h1 className="text-5xl font-bold">Cek Masalah Page</h1>
             </main>
         </div>
